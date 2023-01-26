ce6cf98fe09e34276e4eebab5cdf9db4236871dc
Erwan Ameil <wan@idlewan.com>
2023-01-26 12:48:35 +0000

    Some additional error handling

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState}  from 'react';
 import { createRoot } from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Grid } from '@react-three/drei';
 
@@ -26,31 +27,60 @@ try {
 
 function init() {
     createRoot(document.getElementById('root')!).render(
-        <Canvas camera={{ position: [-6, 0, 5] }} dpr={1} >
-
-            {/* these only light the debug spheres when they are visible */}
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <directionalLight position={[0, 0, 5]} color="red" />
-
-            <MetaballsQuad />
-
-            {/* // useful objects to debug if unsure how camera/things are moving/updating
-                // notably autoRotate
-
-            <axesHelper />
-
-            <Grid renderOrder={-1} infiniteGrid
-                position={[0, -1.85, 0]}
-                cellSize={0.5} sectionSize={2.5}
-                sectionColor={0xffffff}
-                cellThickness={0.5} sectionThickness={1.5}
-                fadeDistance={30} />
-            */}
-
-            <OrbitControls makeDefault
-                autoRotate autoRotateSpeed={0.75} enableZoom={true}
-            />
-        </Canvas>
+        <App />
     );
+}
+type ErrorInfo = { error: Error, info: {componentStack: string} };
+function ShowError(props: ErrorInfo) {
+    return <p className="error">
+        {'' + props.error}
+        <br />
+        {props.info.componentStack}
+    </p>;
+}
+
+function App() {
+    const [state, setState] = useState({} as ErrorInfo);
+    if (state.error) {
+        return <ShowError error={state.error} info={state.info}/>;
+    }
+
+    return <Canvas camera={{ position: [-6, 0, 5] }} dpr={1} >
+        <ErrorBoundary
+            fallbackRender={() => <></>}
+            onError={(error, info) => setState({error, info})}
+        >
+
+            <SceneContent />
+
+        </ErrorBoundary>
+    </Canvas>;
+}
+
+function SceneContent() {
+    return <>
+        {/* these only light the debug spheres when they are visible */}
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <directionalLight position={[0, 0, 5]} color="red" />
+
+        <MetaballsQuad />
+
+        {/* // useful objects to debug if unsure how camera/things are moving/updating
+            // notably autoRotate
+
+        <axesHelper />
+
+        <Grid renderOrder={-1} infiniteGrid
+            position={[0, -1.85, 0]}
+            cellSize={0.5} sectionSize={2.5}
+            sectionColor={0xffffff}
+            cellThickness={0.5} sectionThickness={1.5}
+            fadeDistance={30} />
+        */}
+
+        <OrbitControls makeDefault
+            autoRotate autoRotateSpeed={0.75} enableZoom={true}
+        />
+    </>;
 }
